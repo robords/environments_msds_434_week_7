ffb9dedaf661eb8d6cb9a78f7143c5fe7c433700
Curt Robords <robords@amazon.com>
2022-11-04 18:23:38 -0400

    trying to get API endpoint via os

--- application.py
+++ application.py
@@ -5,11 +5,13 @@
 
 from hello import hello
 from flask import Flask
+import os
 
 application = Flask(__name__)
 @application.route('/')
 def index():
-    return hello('AWS EB!')
+    endpoint = os.environ['API_ENDPOINT']
+    return hello(f'AWS EB! {endpoint}')
 
 if __name__ == '__main__':
     application.run(host='127.0.0.1', port=8080, debug=True)
