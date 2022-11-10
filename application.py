87e652ec61ec92dc30c895c5d1d693bcc97db5fc
Curt Robords <robords@amazon.com>
2022-11-10 17:13:54 -0500

    working on showing forecast data on webpage

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from hello import hello
+from weather_predictions import hello
 from flask import Flask
 import os
 
@@ -6,7 +6,9 @@ application = Flask(__name__)
 @application.route('/')
 def index():
     endpoint = os.environ['API_ENDPOINT']
-    return hello(f'AWS EB! This is the {endpoint} Environment')
+    greeting = hello(f'AWS EB! This is the {endpoint} Environment\n\n')
+    data = snow_forecast_data('FL')
+    return greeting, data 
 
 if __name__ == '__main__':
     application.run(host='127.0.0.1', port=8080, debug=True)
---
+++ weather_predictions.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import boto3
+import json
+
+client = boto3.client('forecast')
+forecastquery = boto3.client(service_name='forecastquery')
+
+def hello(environment):
+    """Return a friendly HTTP greeting."""
+    result = f'Hello {environment}'
+    return result
+
+def snow_forecast_data(state):
+    """Return a friendly HTTP greeting."""
+
+    FORECAST_NAME = "SNOW_FORECAST"
+
+    if FORECAST_NAME in [i['ForecastName'] for i in client.list_forecasts()['Forecasts']]:
+        print('Already Exists')
+        forecast_arn = ([item for item in client.list_forecasts()['Forecasts'] 
+                             if item["ForecastName"] == FORECAST_NAME][0]['ForecastArn'])
+    
+    result = forecastquery.query_forecast(
+        ForecastArn=forecast_arn,
+        Filters={"item_id": state}
+    )
+    
+    return result
