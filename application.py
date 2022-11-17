09dd9c2700e6acf6bf5b708cd06a2abc908fba79
Curt Robords <robords@amazon.com>
2022-11-16 19:04:30 -0500

    remove route

--- application.py
+++ application.py
@@ -87,16 +87,5 @@ def plot_service_p10(service):
 def plot_service_p90(service):
     return plot_service(service, 'p90')
 
-@application.route('/print-plot')
-def plot_png():
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    xs = np.random.rand(100)
-    ys = np.random.rand(100)
-    axis.plot(xs, ys)
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
-
 if __name__ == '__main__':
     application.run(host='127.0.0.1', port=8080, debug=True)
