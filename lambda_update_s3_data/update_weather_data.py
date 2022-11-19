befb21d4a879c0b9f04601dcca85ef82194f1737
Curt Robords <robords@amazon.com>
2022-11-19 18:26:06 -0500

    updating lambda

--- lambda_update_s3_data/update_weather_data.py
+++ lambda_update_s3_data/update_weather_data.py
@@ -1,37 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from lambda_update_s3_data import pandas as pd
-from lambda_update_s3_data import boto3
+import pandas as pd
+import boto3
 import datetime
 from collections import defaultdict
 from io import StringIO
-
-
-def get_most_recent_date_from_s3():
-    '''
-    Here, we get the most recent file from s3, and then get the date from it
-    '''
-    # Here's the URIs
-    bucket = 'raw-weather-data'
-    path_names = ['SNOW', 'PRCP', 'SNWD', 'TMAX', 'TMIN']
-    
-    current_year = datetime.datetime.now().year
-    s3 = boto3.client('s3')
-    response = s3.list_objects_v2(
-            Bucket=bucket,
-            Prefix=path_names[0] # this is somewhat arbitrary, but we just need to pick a path
-        )
-
-    for item in path_names:
-        key = [i['Key'] for i in response['Contents'] if str(current_year) in
-              i['Key']][0]
-        
-    response = s3.get_object(Bucket=bucket, Key=key)
-    most_recent_df = pd.read_csv(response.get("Body"))
-    most_recent_date_available = most_recent_df['date-'].max()
-
-    return most_recent_date_available
 
 
 def get_and_put_data_from_noaa(event, context):
@@ -63,7 +37,6 @@ def get_and_put_data_from_noaa(event, context):
     most_recent_df = most_recent_df.merge(stations, left_on='id', right_on='id')
     most_recent_df.rename(columns={"state": 'location'}, inplace=True)
     # create a separate file for each value in the third column
-    df_dict = defaultdict(dict)
     elements = ['PRCP','SNOW','SNWD','TMAX','TMIN']
     for i in elements:
         # filter the dataframe for just the records with that element
@@ -73,8 +46,6 @@ def get_and_put_data_from_noaa(event, context):
         # update the date field so it's got dashes
         df['date-'] = pd.to_datetime(df['date-'], format='%Y%m%d') 
         df['date-'] = df['date-'].dt.strftime('%Y-%m-%d')
-        # add them to a dict of dataframes
-        df_dict[i] = df
         
         # write it directly to s3
         bucket = 'raw-weather-data'
@@ -82,6 +53,8 @@ def get_and_put_data_from_noaa(event, context):
         df.to_csv(csv_buffer, index=False)
         s3_resource = boto3.resource('s3')
         s3_resource.Object(bucket, f'{i}/{current_year}.csv').put(Body=csv_buffer.getvalue())
+        # This should release it from memory
+        del df
 
 
 
